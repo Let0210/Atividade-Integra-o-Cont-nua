3a80acee725d0324bcc31f4d091f740dc466c6ce
Let0210 <oliveirarodriguesleticiaana@gmail.com>
2023-02-14 15:40:20 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Tomara que funcione. Deus abencoe</h1>
+      <p>Que a força esteja conosco.</p>
+    </div>
+  );
+}
